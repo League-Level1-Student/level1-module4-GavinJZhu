88497d2622a7b16124d11d905dd129910761332c
Gavin <gzhu100@icloud.com>
2023-03-25 11:26:31 -0700

    yews

---
+++ src/_01_chuckle_clicker/ChuckleClicker.java
@@ -0,0 +1,12 @@
+package _01_chuckle_clicker;
+
+import javax.swing.*;
+
+public class ChuckleClicker {
+    public static void main(String[] args) {
+
+    }
+    public static void makeButtons(){
+        JOptionPane.showMessageDialog(null,"make buttons");
+    }
+}
