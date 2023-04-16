7c92e4beeb143f858129ed9c5f2122d59493232e
league <league@imac-14.attlocal.net>
2023-04-16 11:29:04 -0700

    finished book of illusions, working on fortune trllrreere

--- src/_05_fortune_teller/FortuneTeller.java
+++ src/_05_fortune_teller/FortuneTeller.java
@@ -28,7 +28,7 @@ public class FortuneTeller extends JPanel implements Runnable, MouseListener {
         // 3. Complete the begin() method in the FortuneTellerRunner class
         
         // 4. add a mouse listener to the frame
-        
+        frame.addMouseListener(this);
     }
 
     @Override
@@ -37,12 +37,12 @@ public class FortuneTeller extends JPanel implements Runnable, MouseListener {
         int mouseY = e.getY();
         
         // 5. Print the mouseX variable
-        
+        System.out.println(mouseX+mouseY);
         // 6. Add the mouseY variable to the previous line so that it prints out too (no new line)
         
         // 7. Adjust your secret location co-ordinates here:
-        int secretLocationX = 0;
-        int secretLocationY = 0;
+        int secretLocationX = 69;
+        int secretLocationY = 420;
         
         // If the mouse co-ordinates and secret location are close, we'll let them ask a question.
         if (areClose(mouseX, secretLocationX) && areClose(mouseY, secretLocationY)) {
