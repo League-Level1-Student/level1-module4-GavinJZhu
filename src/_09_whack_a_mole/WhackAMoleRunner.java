d8a140dffe5be25d231011e1c5129cf8013466f8
league <league@imac-14.attlocal.net>
2023-05-07 11:29:26 -0700

    a

--- src/_09_whack_a_mole/WhackAMoleRunner.java
+++ src/_09_whack_a_mole/WhackAMoleRunner.java
@@ -1,5 +1,10 @@
 package _09_whack_a_mole;
 
 public class WhackAMoleRunner {
+public static void main(String[] args) {
+	
 
+whack_a_mole whack = new whack_a_mole();
+whack.run();
+}
 }
--- src/_09_whack_a_mole/whack_a_mole.java
+++ src/_09_whack_a_mole/whack_a_mole.java
@@ -1,7 +1,39 @@
 package _09_whack_a_mole;
 
-public class whack_a_mole {
-    public static void main(String[] args) {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
 
-    }
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class whack_a_mole implements ActionListener{
+    JFrame frame = new JFrame();
+    JPanel panel = new JPanel();
+    Random ran = new Random();
+	public void run() {
+	   
+	   frame.add(panel);
+	   frame.setVisible(true);
+	  
+	   frame.setSize(250,325); 
+	   drawButtons(ran.nextInt(24));
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		
+	}
+	public void drawButtons(int i){
+		for (int j = 0; j<24; j++) {
+			   JButton amogus = new JButton();
+			   panel.add(amogus);
+			   amogus.addActionListener(this);
+			   if (j == i) {
+				   amogus.setText("mole!");
+			   }
+		   }
+			
+	}
 }
