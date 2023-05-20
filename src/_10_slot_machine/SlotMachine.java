bcdd14e2f5ec23d2f4e9b31134e6d43b7cf6ef81
Gavin <gzhu100@icloud.com>
2023-05-20 11:29:30 -0700

    whackamole is impossible and starting to work a little on slot machine

---
+++ src/_10_slot_machine/SlotMachine.java
@@ -0,0 +1,26 @@
+package _10_slot_machine;
+
+import javax.swing.*;
+import java.util.Random;
+
+public class SlotMachine {
+    JFrame frame = new JFrame();
+    JPanel panel = new JPanel();
+    JButton spin = new JButton();
+    Random ran = new Random();
+    String cherry = "cherry.png";
+    String lime = "lime.jpg";
+    String orange = "orange.jpg";
+
+    public void run() {
+
+    }
+
+    public int randomizeEachSlot
+
+    {
+        if (ran.nextInt(3) == 0) {
+            panel.add(c);
+        }
+    }
+}
